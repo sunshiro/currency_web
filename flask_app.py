4990d7175a9206f9f9a4c9dcde83c30d3ce1a70a
Natthapong Ju <natthapong.ju@kmitl.ac.th>
2021-11-11 10:58:10 +0000

    My first commit

---
+++ flask_app.py
@@ -0,0 +1,18 @@
+
+# A very simple Flask Hello World app for you to get started with...
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
+
+@app.route('/aaaa')
+def hello_world_aaaa():
+    return 'the path is aaaa'
+
+@app.route('/aaaa/bbbb')
+def hello_world_aaaa_bbbb():
+    return 'the path is aaaa/bbbb'
